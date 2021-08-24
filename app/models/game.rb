260d9737fbf0f38cf6eed649b818ca4fe087fb60
Lukas Möller <lukasmoeller@urlukas.com>
2021-08-24 11:56:35 +0200

    Add validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,12 @@
 class Game < ApplicationRecord
   has_many :play_sessions
+
+  validates :title, presence: true, uniqueness: true
+  validates :instructions, presence: true
+  validates :video, presence: true
+  # Only difficulties:
+  validates :difficulty, presence: true, inclusion:
+    {
+      in: %w[easy medium hard]
+    }
 end
