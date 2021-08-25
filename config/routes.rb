4aed602213fa0cdc4ce3c4295e82918f1d049982
Lukas Möller <lukasmoeller@urlukas.com>
2021-08-25 11:38:36 +0200

    working on flickity

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Put in play_sessions later
+  resources :games, only: :show do
+  end
 end
