a56e97594a6308da44a1fc8048c03371d40a95d5
Lara Klemme <laraklemme@gmail.com>
2021-08-25 18:08:54 +0200

    timer run setup

--- app/javascript/controllers/display_timer_controller.js
+++ app/javascript/controllers/display_timer_controller.js
@@ -4,6 +4,34 @@ export default class extends Controller {
   static targets = ['timer'];
 
   connect() {
-    console.log(this.timer);
+    this.initializeTimer();
+    this.runTimer();
+  }
+
+  initializeTimer() {
+    const startTime = 15;
+
+    this.timerTarget.innerHTML = startTime;
+  }
+
+  runTimer() {
+    setInterval(() => {
+      // stuff that happens every second
+      const currentTime = parseInt(this.timerTarget.innerText);
+      // get the current time that is on the page
+      // convert that to a number
+      if (currentTime > 0) {
+        const newTime = currentTime - 1;
+        // subtract one
+        this.timerTarget.innerHTML = newTime;
+      // change what shows on the page
+      } else {
+        window.location.href = window.location.href + "/options";
+      }
+    }, 1000);
+  }
+
+  pause() {
+
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-<<<<<<< HEAD
-  resources :games, only: :index do
-    resources :play_sessions, only: :create
-  end
-
-  resources :play_sessions, only: [:show, :update]
-=======
   resources :games, only: %i[show index] do
     resources :play_sessions, only: :create
   end
@@ -17,6 +10,5 @@ Rails.application.routes.draw do
     get 'options', to: 'play_sessions#options'
     get 'reward', to: 'play_sessions#reward'
   end
->>>>>>> master
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
