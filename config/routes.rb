724c2456512e393273078371d06391cfb8278b50
Lara Klemme <laraklemme@gmail.com>
2021-08-25 14:45:13 +0200

    stimulus and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :games, only: :index
+  resources :games, only: :index do
+    resources :play_sessions, only: :create
+  end
+
+  resources :play_sessions, only: [:show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
