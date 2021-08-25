f44a894f7de88bfe6a136ecfff1175794008369a
Lara Klemme <laraklemme@gmail.com>
2021-08-25 16:48:47 +0200

    fixed bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 
 
 
-random_user1 = User.create(email: 'abc@gmail.com', password: '1234567', first_name: 'Martin', last_name: 'Jones')
-random_user2 = User.create(email: 'k.eilers@gmail.com', password: '111111', first_name: 'Karin', last_name: 'Eilers')
+random_user1 = User.create(email: 'abc@gmail.com', password: '1234567', nickname: 'Kobolt')
+random_user2 = User.create(email: 'k.eilers@gmail.com', password: '111111', nickname: 'Eilers')
 
 
-Game.create(video: 'QTB5EhxXW-w', title: 'Puckered Lips', instructions: 'Stretch out your lips and form a circle.' difficulty: 'easy' )
+Game.create(video: 'QTB5EhxXW-w', title: 'Puckered Lips', instructions: 'Stretch out your lips and form a circle.', difficulty: 'easy' )
 
-Game.create(video: '1NJPH_x_GJ4', title: 'Kiss and Smile', instructions: 'Push your lips forward and then smile.' difficulty: 'easy' )
+Game.create(video: '1NJPH_x_GJ4', title: 'Kiss and Smile', instructions: 'Push your lips forward and then smile.', difficulty: 'easy' )
 
-Game.create(video: '2a48kSakDLI', title: 'Teeth Toucher', instructions: 'Let the tongue touch all your teeth individually.' difficulty: 'medium' )
+Game.create(video: '2a48kSakDLI', title: 'Teeth Toucher', instructions: 'Let the tongue touch all your teeth individually.', difficulty: 'medium' )
 
-Game.create(video: '-b1nl7jO8JQ', title: 'The Waving Tongue', instructions: 'Point the tongue towards your nose and chin.' difficulty: 'medium' )
+Game.create(video: '-b1nl7jO8JQ', title: 'The Waving Tongue', instructions: 'Point the tongue towards your nose and chin.', difficulty: 'medium' )
 
-Game.create(video: '-B24LqnDRb4', title: 'Clicking Tongue', instructions: 'Suck your tongue onto your palate and release it with a bang.' difficulty: 'hard' )
+Game.create(video: '-B24LqnDRb4', title: 'Clicking Tongue', instructions: 'Suck your tongue onto your palate and release it with a bang.', difficulty: 'hard' )
