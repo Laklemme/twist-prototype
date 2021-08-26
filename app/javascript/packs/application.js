ca26f498f7425679e99f2b2063d705907ebcca92
Philipp Bruns <plath.philipp@gmail.com>
2021-08-26 14:52:55 +0200

    hippo-button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,12 +23,12 @@ require("channels")
 // ----------------------------------------------------
 
 // External imports
-import "bootstrap";
+//import "bootstrap";
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { initiateHippoButton } from '../components/hippo-button';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initiateHippoButton();
 });
