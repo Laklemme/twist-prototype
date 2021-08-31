8e9de5721e6ce2a0aae6f3c518c8f5d00eac9b7f
Lukas Möller <79029266+lmllr@users.noreply.github.com>
2021-08-31 17:22:44 +0200

    Update dashboards_controller.rb

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,11 +1,7 @@
 class DashboardsController < ApplicationController
   def dashboard
     @streak = current_user.streak(:play_sessions)
-  end
-
-  private
-
-  def set_redis_streak_counter
+    
     @users = User.all
     @users = @users.sort {|a,b| a.balance <=> b.balance}.reverse
   end
