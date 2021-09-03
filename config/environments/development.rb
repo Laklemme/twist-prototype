13a45e38ea64f82da3fc77c48dd29f53c5ac35eb
Lara Klemme <laraklemme@gmail.com>
2021-09-03 12:26:27 +0200

    design tings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.hosts << "5e2e-62-96-151-154.ngrok.io"
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
